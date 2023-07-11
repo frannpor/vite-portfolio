39cf6b104245ae1a0428b878fe6f618078d33330
frannpor <frannporciel@gmail.com>
2023-07-11 03:11:26 -0300

    set-up landing component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Main from './components/Main'
 import Sidenav from './components/Sidenav'
+
 
 export default function App() {
   return (
     <div>
       <Sidenav />
+      <Main />
     </div>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,49 @@
+import me from "../assets/me.png";
+import { TypeAnimation } from "react-type-animation";
+import { FaTwitter, FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+
+const Main = () => {
+  return (
+    <div id="main">
+      <img
+        className="w-full h-screen object-cover object-left"
+        src={me}
+        alt="Francisco Porciel"
+      />
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/50 select-none">
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
+            I'm Francisco Porciel
+          </h1>
+          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
+            I'm a
+            <TypeAnimation
+              sequence={[
+                "Developer",
+                1000,
+                "Coder",
+                1000,
+                "Tech enthusiast",
+                1000,
+                "Lol enjoyer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{ fontSize: "1em", paddingLeft: "5px" }}
+              repeat={Infinity}
+            />
+          </h2>
+          <div className="flex justify-between pt-6 max-w-[200px] w-full">
+            <FaTwitter className="cursor-pointer" size={20} />
+            <FaFacebook className="cursor-pointer" size={20} />
+            <FaInstagram className="cursor-pointer" size={20} />
+            <FaLinkedin className="cursor-pointer" size={20} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
